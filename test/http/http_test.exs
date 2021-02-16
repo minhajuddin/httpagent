5e5e3f2aa2a8ccb2b8a57dacb80b572cc9b79c61
Khaja Minhajuddin <minhajuddin.k@gmail.com>
2021-02-15 22:32:17 -0500

    Fmt

--- test/http/http_test.exs
+++ test/http/http_test.exs
@@ -3,24 +3,38 @@ defmodule HA.HTTPTest do
 
   alias HA.HTTP
 
-  test "when url is invalid" do
-    for url <- ~w[
-      htts://this-is-a-non-existent-domain.nonexistentdomain
-      hs://this-is-a-non-existent-domain.nonexistentdomain
-      ://this-is-a-non-existent-domain.nonexistentdomain
-      this-is-a-non-existent-domain.nonexistentdomain
-      http://:this-is-a-non-existent-domain.nonexistentdomain
-      carrot://foo:80this-is-a-non-existent-domain.nonexistentdomain
-    ] do
-      assert {:error, error} = HTTP.request(:get, url)
+  describe "error conditions return an error response" do
+    test "when url is invalid" do
+      for url <- ~w[
+        htts://this-is-a-non-existent-domain.nonexistentdomain
+        hs://this-is-a-non-existent-domain.nonexistentdomain
+        ://this-is-a-non-existent-domain.nonexistentdomain
+        this-is-a-non-existent-domain.nonexistentdomain
+        http://:this-is-a-non-existent-domain.nonexistentdomain
+        carrot://foo:80this-is-a-non-existent-domain.nonexistentdomain
+      ] do
+        assert {:error, error} = HTTP.request(:get, url)
 
-      assert error.message =~
-               ~r/invalid scheme|the :hostname option is required when address is not a binary/
+        assert error.message =~
+                 ~r/invalid scheme|the :hostname option is required when address is not a binary/
+      end
+    end
+
+    test "when domain does not exist" do
+      assert {:error, error} = HTTP.request(:get, "http://dangcom.carrotinsta/")
+      assert error.reason == :nxdomain
+    end
+
+    test "when server refuses connections" do
+      assert {:error, error} = HTTP.request(:get, "http://localhost:4082/")
+      assert error.reason == :econnrefused
     end
   end
 
-  test "when domain does not exist" do
-    assert {:error, error} = HTTP.request(:get, "http://dangcom.carrotinsta/")
-    assert error.reason == :nxdomain
+  describe "timeouts" do
+    test "when we timeout connecting" do
+      assert {:error, error} = HTTP.request(:get, "http://google.com:4082/")
+      assert error == "test"
+    end
   end
 end
